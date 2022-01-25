9d13a82bef1dfdc2542a68828b341aadf3a8902b
kira0x1 <kira.eva@protonmail.com>
2022-01-25 08:31:17 +0300

    added some structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+    let input = "2 * 7 + 5";
+    get_tokens(input);
+}
+
+#[derive(Debug)]
+struct Token {
+    token_type: TokenType,
+}
+
+#[derive(Debug)]
+enum TokenType {
+    Num,
+    Plus,
+    Times,
+    Identifier,
+    True,
+    False,
+    EOF,
+}
+
+fn get_tokens(input: &str) {
+    let mut tokens: Vec<Token> = Vec::new();
+    tokens.push(Token {
+        token_type: TokenType::Identifier,
+    });
+
+    println!("{:?}", tokens);
 }
