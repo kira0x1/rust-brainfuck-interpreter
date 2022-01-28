e812a4fa7400e9d2638569549f89b5f7e4ee4adb
kira0x1 <kira.eva@protonmail.com>
2022-01-28 23:36:41 +0300

    cleaned up lexer loop

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ struct Token {
     start_pos: usize,
 }
 
-#[derive(Debug)]
+#[derive(PartialEq, Eq, Debug)]
 enum TokenType {
     Num,
     Plus,
@@ -18,7 +18,9 @@ enum TokenType {
     Identifier,
     True,
     False,
+    WhiteSpace,
     EOF,
+    UNKNOWN,
 }
 
 fn lexer(input: &str) {
@@ -26,30 +28,18 @@ fn lexer(input: &str) {
     let mut cur_pos = 0;
 
     while cur_pos < input.len() {
-        let mut token_start = cur_pos;
-
+        let token_start = cur_pos;
         let look_ahead = input.chars().nth(cur_pos).unwrap_or(' ');
+        let token_type = get_token_type(look_ahead);
 
-        if look_ahead == ' ' {
-            cur_pos += 1;
-        } else if look_ahead == '+' {
-            cur_pos += 1;
+        if token_type == TokenType::UNKNOWN {
+            println!("unknown characters {} at position {}", look_ahead, cur_pos);
+            break;
+        }
 
-            tokens.push(Token {
-                token_type: TokenType::Plus,
-                text: look_ahead.to_string(),
-                start_pos: token_start,
-            });
-        } else if look_ahead == '*' {
-            cur_pos += 1;
-
-            tokens.push(Token {
-                token_type: TokenType::Times,
-                text: look_ahead.to_string(),
-                start_pos: token_start,
-            });
-        } else if look_ahead.is_numeric() {
+        if token_type == TokenType::Num {
             let mut text: String = "".to_owned();
+
             while cur_pos < input.len() && input.chars().nth(cur_pos).unwrap_or(' ').is_numeric() {
                 text.push_str(&input.chars().nth(cur_pos).unwrap_or(' ').to_string());
                 cur_pos += 1;
@@ -59,17 +49,47 @@ fn lexer(input: &str) {
                     start_pos: token_start,
                 })
             }
-        } else {
-            println!("unknown characters {} at position {}", look_ahead, cur_pos);
-            break;
+            continue;
+        }
+
+        cur_pos += 1;
+
+        if token_type == TokenType::EOF {
+            tokens.push(Token {
+                token_type: TokenType::EOF,
+                text: "<EOF>".to_owned(),
+                start_pos: cur_pos,
+            });
+
+            continue;
         }
 
         tokens.push(Token {
-            token_type: TokenType::EOF,
-            text: "<EOF>".to_owned(),
-            start_pos: cur_pos,
-        })
+            token_type: token_type,
+            text: look_ahead.to_string(),
+            start_pos: token_start,
+        });
     }
 
     println!("{:?}", tokens);
+}
+
+fn get_token_type(input: char) -> TokenType {
+    if input == ' ' {
+        return TokenType::WhiteSpace;
+    }
+
+    if input == '+' {
+        return TokenType::Plus;
+    }
+
+    if input == '*' {
+        return TokenType::Times;
+    }
+
+    if input.is_numeric() {
+        return TokenType::Num;
+    }
+
+    return TokenType::UNKNOWN;
 }
